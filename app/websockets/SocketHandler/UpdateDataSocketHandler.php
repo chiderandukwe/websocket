c640725d3fee71884e07cbe7481757976447b5cd
Chidera <chiderandkwe96@gmail.com>
2023-12-21 12:26:13 +0100

    change

--- app/websockets/SocketHandler/UpdateDataSocketHandler.php
+++ app/websockets/SocketHandler/UpdateDataSocketHandler.php
@@ -5,12 +5,19 @@ namespace App\websockets\SocketHandler;
 use Ratchet\ConnectionInterface;
 use Ratchet\RFC6455\Messaging\MessageInterface;
 use Ratchet\WebSocket\MessageComponentInterface;
+use App\Models\User;
 
 class UpdateDataSocketHandler extends BaseSocketHandler implements MessageComponentInterface
 {
 
     public function onMessage(ConnectionInterface $connection, MessageInterface $msg)
     {
-        // TODO: Implement onMessage() method.
+        User::create(
+            [
+                'name' => $msg,
+                'email' => "my email",
+                'password' => "my datet",
+            ]
+        );
     }
 }
